d4479ff173cdb673b279140f9456ecfc98bfc2f3
JJTM20 <jjtm20@student.aau.dk>
2025-02-13 15:33:02 +0100

    Added all modules, DB handler, classes and initial REST api

--- ML/Trainer.py
+++ ML/Trainer.py
@@ -1,13 +1,22 @@
+import datetime
+class Model:
+    def __init__(self, name, binary, serviceId):
+        self.name = name
+        self.binary = binary
+        self.trainedTime = datetime.date.today()
+        self.serviceId = serviceId
+
 class Trainer:
-    model = None
-    def __init__(self, model):
-        # Train model
-        self.model = model
-        # Return trained model
+    trained_models = {}
+    def __init__(self, models, serviceId):
+        self.models = models
+        self.serviceId = serviceId
         pass
 
     def train_model(self):
-        # Train model using Darts
-        # create new forecast using new trained model
-        # return trained model
-        pass
+        for model in self.models:
+            # 1. Train model using Darts
+            # 2. return trained model
+            self.trained_models.add(Model(model, None, self.serviceId))
+        return self.trained_models
+        
--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 from REST.api import start_api
-from Database.dbhandler import DbConnection, ModelRepository, ForecastRepository
+from Database.dbhandler import DbConnection
 from threading import Thread
 
 def insert_model():
-    model_path = "Assets\\autotheta_model.pth"
-    res = model_repository.insert_model("AutoThetaTest2", model_path)
-    print(f"Inserted: {res}")
+    # model_path = "Assets\\autotheta_model.pth"
+    # res = model_repository.insert_model("AutoThetaTest2", model_path)
+    # print(f"Inserted: {res}")
+    pass
 
-if __name__ == '__main__':
-    db = DbConnection("p10s", "postgres", "password", "localhost", 5432)
-    model_repository = ModelRepository(db)
-    forecast_repository = ForecastRepository(db)
-    #start_api()
-    insert_model()
-
+if __name__ == '__main__':    
+    start_api()
