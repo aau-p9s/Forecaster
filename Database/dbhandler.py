6a2a591daddd52c5bfd1d636a6d3c0d625d975c7
Thomas Jensen <32744171+Mast3rwaf1z@users.noreply.github.com>
2025-03-20 15:10:01 +0100

    fixed database rollback and commented out database access with placeholder data that fails anyways (#31)

--- Database/dbhandler.py
+++ Database/dbhandler.py
@@ -3,7 +3,6 @@ import psycopg2
 class DbConnection:
     def __init__(self, dbname, username, password, hostname, port):
         self.connection = psycopg2.connect(database=dbname, user=username, password=password, host=hostname, port=port)
-        self.cursor = self.connection.cursor()
     
     def execute_query(self, query_string, params=None):
         """Executes query given a querystring and optional parameters.
@@ -11,13 +10,22 @@ class DbConnection:
             query_string: Sql query statement
             params: Query parameters
         """
-        if params == None:
-            self.cursor.execute(query_string)
-        else:
-            self.cursor.execute(query_string, params)
-        data = self.cursor.fetchall()
-        self.connection.commit()
-        return data
+        try:
+            cursor = self.connection.cursor()
+            if params == None:
+                cursor.execute(query_string)
+            else:
+                cursor.execute(query_string, params)
+            data = cursor.fetchall()
+            self.connection.commit()
+            return data
+        except:
+            print("There was an error in the database")
+            cursor = self.connection.cursor()
+            cursor.execute("ROLLBACK")
+            self.connection.commit()
+            return None
+
     
     def close(self):
         """Closes the database connection."""
