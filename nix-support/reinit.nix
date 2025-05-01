5041c93f6a8e942b777602ea2e8b3a26e2eb58b6
Thomas Jensen <mast3r@skade.dev>
2025-05-01 14:46:58 +0200

    updated some scripts

--- nix-support/reinit.nix
+++ nix-support/reinit.nix
@@ -8,9 +8,9 @@ writeScriptBin "reinit" ''
 
     export PGPASSWORD=${postgres_password}
     echo "dropping db"
-    ${postgresql}/bin/dropdb -h localhost -U ${postgres_user} ${postgres_database} --no-password
+    ${postgresql}/bin/dropdb -h ${postgres_address} -U ${postgres_user} ${postgres_database} --no-password
     echo "initializing db"
-    ${postgresql}/bin/createdb -h localhost -U ${postgres_user} ${postgres_database} --no-password
+    ${postgresql}/bin/createdb -h ${postgres_address} -U ${postgres_user} ${postgres_database} --no-password
 
     rm -fr /tmp/autoscaler
     ${pkgs.git}/bin/git clone https://github.com/aau-p9s/Autoscaler /tmp/autoscaler
