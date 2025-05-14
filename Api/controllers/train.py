324b8982df2c2cd0a7be50fdaac99db51d3ef5b0
Thomas Jensen <mast3r@skade.dev>
2025-05-14 21:07:25 +0200

    force making forecasts

--- Api/controllers/train.py
+++ Api/controllers/train.py
@@ -14,12 +14,13 @@ trainers:dict[str, Trainer] = {}
 class Train(Resource):
     @api.doc(params={"serviceId":"your-service-id"}, responses={200:"ok", 202:"working...", 500:"Something ML died!!!!"})
     def post(self, service_id, forecast_horizon=12):
-        historical:list[Historical] = historical_repository.get_by_service(service_id)
-        if not historical:
-            return Response(status=400, response=dumps({"message":f"Error, historical table is empty for service: {service_id}"}))
-
         if not service_id in trainers:
             trainers[service_id] = Trainer(service_id, model_repository, forecast_repository)
+
+        historical:list[Historical] = historical_repository.get_by_service(service_id)
+        if not historical:
+            trainers[service_id].forecaster.predict(None, service_id)
+            return Response(status=400, response=dumps({"message":f"Error, historical table is empty for service: {service_id}"}))
 
         trainers[service_id].train(historical[0], forecast_horizon)
 
