1387e72656161cf850a8dbb53930eeda0fc153ed
Thomas Jensen <mast3r@skade.dev>
2025-05-14 21:55:19 +0200

    stupid error

--- Api/controllers/train.py
+++ Api/controllers/train.py
@@ -12,14 +12,14 @@ trainers:dict[str, Trainer] = {}
 
 @api.route("/train/<string:service_id>/<int:forecast_horizon>")
 class Train(Resource):
-    @api.doc(params={"serviceId":"your-service-id"}, responses={200:"ok", 202:"working...", 500:"Something ML died!!!!"})
+    @api.doc(params={"service_id":"your-service-id"}, responses={200:"ok", 202:"working...", 500:"Something ML died!!!!"})
     def post(self, service_id, forecast_horizon=12):
         if not service_id in trainers:
             trainers[service_id] = Trainer(service_id, model_repository, forecast_repository)
 
         historical:list[Historical] = historical_repository.get_by_service(service_id)
         if not historical:
-            trainers[service_id].forecaster.predict(None, service_id)
+            trainers[service_id].forecaster.predict(None, forecast_horizon)
             return Response(status=400, response=dumps({"message":f"Error, historical table is empty for service: {service_id}"}))
 
         trainers[service_id].train(historical[0], forecast_horizon)
