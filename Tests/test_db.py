1cb71c711e0a4e7c15ceadeedb0f4a4a213e51bc
Thomas Jensen <mast3r@skade.dev>
2025-05-16 10:07:34 +0200

    fix tests?

--- Tests/test_db.py
+++ Tests/test_db.py
@@ -8,6 +8,8 @@ from Database.ModelRepository import ModelRepository
 from Database.Models.Forecast import Forecast
 from Database.Models.Historical import Historical
 from Database.Models.Model import Model
+from Database.Utils import gen_uuid
+from ML.Darts.Utils.preprocessing import scaler
 
 @pytest.fixture
 def mock_db():
@@ -19,13 +21,15 @@ def mock_db():
 def test_get_all_models_by_service(mock_db):
     """Test fetching models for a service with a mocked database"""
     model_obj = NaiveSeasonal()
-    model = Model("model-id", model_obj, "service")
+    model_id = gen_uuid()
+    service_id = gen_uuid()
+    model = Model(model_id, "NaiveSeasonal", model_obj, service_id, None)
     mock_db.execute_get.return_value = [
         ("model-id", type(model_obj).__name__, model.get_binary(), model.serviceId)
     ]
 
     model_repo = ModelRepository(mock_db)
-    result = model_repo.get_all_models_by_service("service")
+    result = model_repo.get_all_models_by_service(service_id)
 
     mock_db.execute_get.assert_called_once_with(
         'SELECT id, name, bin from models WHERE "serviceid" = %s;', [model.serviceId]
@@ -51,12 +55,14 @@ def test_get_all_models_by_service(mock_db):
 
 def test_get_historical(mock_db):
     """Test getting the latest forecast"""
-    historical = Historical("historical-id", "service", 0.0, "{'data':'xd'}")
+    historical_id = gen_uuid()
+    service_id = gen_uuid()
+    historical = Historical(historical_id, service_id, 0.0, {'data':'xd'})
     mock_db.execute_get.return_value = [
         (historical.id, historical.service_id, historical.timestamp, historical.data)
     ]
     
     repo = HistoricalRepository(mock_db)
-    result = repo.get_by_service("service")
+    result = repo.get_by_service(service_id)
 
     assert result[0].data == historical.data
