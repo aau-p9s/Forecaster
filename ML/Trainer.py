13a2d58c31731f9de12f0e6650b8f979adbe3093
Thomas Jensen <mast3r@skade.dev>
2025-06-05 01:28:09 +0200

    changed back to non-mp

--- ML/Trainer.py
+++ ML/Trainer.py
@@ -1,33 +1,32 @@
 
 
-from datetime import timedelta
 from multiprocessing import Process
 from multiprocessing.managers import DictProxy
 from uuid import UUID
 
 from darts import TimeSeries
-from darts.models.forecasting.forecasting_model import ForecastingModel
 
 from Database.ForecastRepository import ForecastRepository
 from Database.ModelRepository import ModelRepository
 from Database.SettingsRepository import SettingsRepository
 from Database.Models.Historical import Historical
 from Database.Models.Model import Model
-from ML.Darts.Utils.preprocessing import load_historical_data, load_json_data, run_transformer_pipeline
+from ML.Darts.Utils.preprocessing import load_historical_data, run_transformer_pipeline
 from ML.Darts.Utils.timeout import timeout
 from ML.Forecaster import Forecaster
 import multiprocessing as mp
 
 
 class Trainer:
-    manager = mp.Manager()
+    #manager = mp.Manager()
     def __init__(self, service_id:UUID, model_repository:ModelRepository, forecast_repository:ForecastRepository, settings_repository:SettingsRepository) -> None:
         self.id:UUID = service_id
         self.model_repository:ModelRepository = model_repository
         self.forecast_repository:ForecastRepository = forecast_repository
         self.settings_repository:SettingsRepository = settings_repository
         self.forecaster = Forecaster(service_id, model_repository, forecast_repository, settings_repository)
-        self.model_status = self.manager.dict()
+        #self.model_status = self.manager.dict()
+        self.model_status = {}
 
     def train(self, series:Historical, horizon:int) -> None:
         self.model_status.clear()
@@ -46,19 +45,29 @@ class Trainer:
         for model in models:
             self.model_status[model.name] = "working"
 
-        with mp.Pool(4) as p:
-            fitted_models = p.map(train_one, [(model, train_series, self.model_status) for model in models])
+        #with mp.Pool(4) as p:
+        #    fitted_models = p.map(train_model, [(model, train_series, self.model_status) for model in models])
 
-        successfully_fitted_models = list(filter(lambda model: model is not None, fitted_models))
-        for model in successfully_fitted_models:
-            if model is not None:
-                self.model_repository.upsert_model(model)
+        #successfully_fitted_models = list(filter(lambda model: model is not None, fitted_models))
+        #for model in successfully_fitted_models:
+        #    if model is not None:
+        #        self.model_repository.upsert_model(model)
 
-        print(f"Successfully fitted {len(successfully_fitted_models)}", flush=True)
+        for model in models:
+            try:
+                fitted_model = model.model.fit(train_series)
+                self.model_status[model.name] = "finished"
+                self.model_repository.upsert_model(Model(model.modelId, model.name, fitted_model, model.serviceId, model.scaler))
+            except Exception as e:
+                self.model_status[model.name] = "failed"
+                print(e)
 
+
+
+        print("Finished training", flush=True)
         self.forecaster._predict(validation_series, period)
 
-def train_one(args:tuple[Model, TimeSeries, DictProxy]) -> Model | None:
+def train_model(args:tuple[Model, TimeSeries, DictProxy]) -> Model | None:
     model, series, status_dict = args
     print(f"Training model: {model.name}", flush=True)
     try:
