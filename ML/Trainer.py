4940b47a3c904eecdaf02d52602fa9ee0da13d53
Thomas Jensen <mast3r@skade.dev>
2025-06-04 23:14:40 +0200

    fix timeout

--- ML/Trainer.py
+++ ML/Trainer.py
@@ -70,11 +70,11 @@ def signal_handler(arg1, arg2):
     raise RuntimeError("Timed out!")
 
 def timeout(f, time=300):
-    def wrapper():
+    def wrapper(*kwargs):
         signal.signal(signal.SIGALRM, signal_handler)
         signal.alarm(time)
         try:
-            return f()
+            return f(*kwargs)
         except RuntimeError as e:
             return None
     return wrapper
