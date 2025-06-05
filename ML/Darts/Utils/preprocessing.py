331931224334cb7268d8b1e7fe3769421a104413
Thomas Jensen <mast3r@skade.dev>
2025-06-05 22:33:57 +0200

    FIXED!!!!!!!!!

--- ML/Darts/Utils/preprocessing.py
+++ ML/Darts/Utils/preprocessing.py
@@ -107,7 +107,7 @@ def load_historical_data(data:Historical, period:int) -> TimeSeries:
     }
     df = pd.DataFrame(series)
     df['timestamp'] = pd.to_datetime(df['timestamp']).dt.tz_localize(None)
-    final_series = TimeSeries.from_dataframe(df, time_col='timestamp', value_cols='value', fill_missing_dates=True, freq=f"{period/60}s").astype(np.float32)
+    final_series = TimeSeries.from_dataframe(df, time_col='timestamp', value_cols='value', fill_missing_dates=True, freq=f"{period/65}s").astype(np.float32)
     return final_series
 
 
