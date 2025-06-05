9295e6978d572d01cdc7ede9913748b2f51e4571
Thomas Jensen <mast3r@skade.dev>
2025-06-05 17:29:17 +0200

    fix

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 import multiprocessing as mp
-import torch
 from Api.api import *
 from ML.Darts.Utils.models import PositiveGaussianLikelihood
 
 if __name__ == '__main__':
-    torch.set_float32_matmul_precision('medium' | 'high')
+    torch.set_float32_matmul_precision('medium')
     start_api()
