385ff78138cd246bea08e66d36da61dda88cab87
Thomas Jensen <mast3r@skade.dev>
2025-06-06 03:20:37 +0200

    reinit db connection if its died

--- Database/dbhandler.py
+++ Database/dbhandler.py
@@ -5,16 +5,16 @@ import psycopg2
 class DbConnection:
     def __init__(self, dbname, username, password, hostname, port):
         self.connection = psycopg2.connect(database=dbname, user=username, password=password, host=hostname, port=port)
-        #self.lock = Lock()
+        self.lock = Lock()
         self.database = dbname
         self.user = username
         self.password = password
         self.host = hostname
         self.port = port
     def execute(self, query_string, params=None) -> None:
-        #print("Getting lock", flush=True)
-        #self.lock.acquire()
-        #print("Got lock", flush=True)
+        print("Getting lock", flush=True)
+        self.lock.acquire()
+        print("Got lock", flush=True)
         if self.connection.closed:
             self.__init__(self.database, self.user, self.password, self.host, self.port)
         try:
@@ -30,8 +30,8 @@ class DbConnection:
             cursor = self.connection.cursor()
             cursor.execute("ROLLBACK")
             self.connection.commit()
-        #self.lock.release()
-        #print("Released lock")
+        self.lock.release()
+        print("Released lock")
 
     def execute_get(self, query_string, params=None) -> list[tuple]:
         """Executes query given a querystring and optional parameters.
@@ -39,9 +39,9 @@ class DbConnection:
             query_string: Sql query statement
             params: Query parameters
         """
-        #print("Getting lock", flush=True)
-        #self.lock.acquire()
-        #print("Got lock", flush=True)
+        print("Getting lock", flush=True)
+        self.lock.acquire()
+        print("Got lock", flush=True)
         if self.connection.closed:
             self.__init__(self.database, self.user, self.password, self.host, self.port)
         try:
@@ -52,8 +52,8 @@ class DbConnection:
                 cursor.execute(query_string, params)
             data = cursor.fetchall()
             self.connection.commit()
-            #self.lock.release()
-            #print("Released lock", flush=True)
+            self.lock.release()
+            print("Released lock", flush=True)
             return data
         except Exception:
             print("There was an error in the database", flush=True)
@@ -61,8 +61,8 @@ class DbConnection:
             cursor = self.connection.cursor()
             cursor.execute("ROLLBACK")
             self.connection.commit()
-            #self.lock.release()
-            #print("Released lock", flush=True)
+            self.lock.release()
+            print("Released lock", flush=True)
             return []
 
     
